bb8dfe19f0d5599fcb8ff28f94819c66589c80a9
c4chris124 <c4chris124@gmail.com>
2022-02-02 11:00:34 -0600

    Adding profile View component

---
+++ src/Views/ProfileView/ProfileView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfileView = () => {
+    return (
+        <div>
+            <h1>I'm ProfileView</h1>
+        </div>
+    )
+};
+
+export default ProfileView;
