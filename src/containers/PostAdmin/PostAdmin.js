4b631114a3a726c8ea590bf95dfb1c90393f58e3
Nicolas Robledo <87320471+NicoRob92@users.noreply.github.com>
2022-02-06 03:23:09 -0300

    muchas cosas

--- src/containers/PostAdmin/PostAdmin.js
+++ src/containers/PostAdmin/PostAdmin.js
@@ -4,24 +4,26 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getPosts } from '../../ducks/actions/actionCreators';
 import {NavLink} from 'react-router-dom'
 import { ModalDeletePost } from '../../components/Modals/ModalDeletePost';
+import Paginado from '../Post/paginado'
+
 export const PostAdmin = () => {
   const array = useSelector((state) => state.reducer.posts);
-  const Post = array?.filter((e) => e.User.role === 'admin');
-
+  const Post = array
   const dispatch = useDispatch();
   const [filter, setFilter] = useState(null);
-  const [toShow, setToShow] = useState(null);
   const [show, setShow] = useState(true);
   const [deletePost, setDeletePost] = useState(false);
-
+  const [current, setCurrent] = useState(1)
+  const lastIndex = current * 27
+  const first = lastIndex - 27
+  let toShow = filter ? filter?.slice(first, lastIndex) : array.slice(first, lastIndex)
   const [id,setId] = useState(null);
+  const pages = filter?.length || array?.length / 27
   useEffect(() => {
     dispatch(getPosts());
   }, [dispatch]);
 
-  useEffect(() => {
-    Post ? setToShow(Post.slice(0, 27)) : setToShow(null);
-  }, [array]);
+
 
   const [input, setInput] = useState({
     option: '',
@@ -39,7 +41,19 @@ export const PostAdmin = () => {
     deletePost ? setDeletePost(!deletePost) : setDeletePost(deletePost);
   };
   
+  const paginate = (e) => {
+    setCurrent(e)
+  }
+  const next = (e) => {
+    e.preventDefault();
+    if (current < pages) setCurrent((current) => current + 1);
 
+  };
+  const prev = (e) => {
+    e.preventDefault();
+    if (current > 1) setCurrent((current) => current - 1);
+
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -55,6 +69,7 @@ export const PostAdmin = () => {
     setFilter(null);
   };
 
+  
   // const setPage = (e) => setCurrent((prevState) => (prevState = e.target.value));
   return (
     <div className={styles.container}>
@@ -114,13 +129,7 @@ export const PostAdmin = () => {
         <div className={styles.boxOne}>Lista de Post</div>
         <div className={styles.boxTwo}>
           <div className={styles.title}>ID Post</div>
-          {filter ? (
-            filter?.map((e) => (
-              <div className={styles.item} key={e.id}>
-                {e.id}
-              </div>
-            ))
-          ) : toShow ? (
+         { toShow ? (
             toShow.map((e) => (
               <div className={styles.item} key={e.id}>
                 {e.id}
@@ -132,13 +141,7 @@ export const PostAdmin = () => {
         </div>
         <div className={styles.boxThree}>
           <div className={styles.title}>Title</div>
-          {filter ? (
-            filter?.map((e) => (
-              <div className={styles.item} key={e.id}>
-                {e.name}
-              </div>
-            ))
-          ) : toShow ? (
+          {toShow ? (
             toShow.map((e) => (
               <div className={styles.item} key={e.id}>
                 {e.name}
@@ -150,14 +153,7 @@ export const PostAdmin = () => {
         </div>
         <div className={styles.boxFour}>
           <div className={styles.title}>Link</div>
-          {filter ? (
-            filter?.map((e) => (
-              <div key={e.id} className={styles.item}>
-                <a
-                  href={`http://localhost:3000/detail/${e.id}`}>{`http://localhost:3000/detail/${e.id}`}</a>
-              </div>
-            ))
-          ) : toShow ? (
+          {toShow ? (
             toShow.map((e) => (
               <div key={e.id} className={styles.item}>
                 <a
@@ -170,15 +166,7 @@ export const PostAdmin = () => {
         </div>
         <div className={styles.boxFive}>
           <div className={styles.title}>Author</div>
-          {filter ? (
-            filter?.map((e) => (
-              <div
-                key={e.id}
-                className={
-                  styles.item
-                }>{`Id: ${e.User.id}  User: ${e.User.username}`}</div>
-            ))
-          ) : toShow ? (
+          {toShow ? (
             toShow.map((e) => (
               <div
                 key={e.id}
@@ -192,95 +180,7 @@ export const PostAdmin = () => {
         </div>
         <div className={styles.boxSix}>
           <div className={styles.title}>Action</div>
-          {filter ? (
-            filter.map((e) => (
-              <div key={e.id} className={styles.actions}>
-                <button>
-                  <NavLink to={`/user/editpost/${e.id}`}>
-                  <svg
-                    width='25'
-                    height='25'
-                    viewBox='0 0 33 33'
-                    fill='none'
-                    xmlns='http://www.w3.org/2000/svg'>
-                    <path
-                      d='M12.7125 27.1647H6.71253C6.44731 27.1647 6.19296 27.0593 6.00542 26.8718C5.81789 26.6843 5.71253 26.4299 5.71253 26.1647V20.5772C5.71208 20.4473 5.73723 20.3187 5.78657 20.1986C5.8359 20.0784 5.90844 19.9692 6.00003 19.8772L21 4.87719C21.0931 4.7827 21.204 4.70767 21.3263 4.65645C21.4486 4.60523 21.5799 4.57886 21.7125 4.57886C21.8451 4.57886 21.9764 4.60523 22.0987 4.65645C22.2211 4.70767 22.332 4.7827 22.425 4.87719L28 10.4522C28.0945 10.5452 28.1695 10.6561 28.2208 10.7785C28.272 10.9008 28.2984 11.0321 28.2984 11.1647C28.2984 11.2973 28.272 11.4286 28.2208 11.5509C28.1695 11.6732 28.0945 11.7841 28 11.8772L12.7125 27.1647Z'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M17.7125 8.16467L24.7125 15.1647'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M27.7125 27.1647H12.7125L5.77502 20.2272'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M21.2125 11.6647L9.21252 23.6647'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                  </svg>
-                  </NavLink>
-                </button>               
-                <button>
-                  <svg
-                    width='25'
-                    height='25'
-                    viewBox='0 0 33 33'
-                    fill='none'
-                    xmlns='http://www.w3.org/2000/svg'>
-                    <path
-                      d='M27.7125 7.16467H5.71252'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M13.7125 13.1647V21.1647'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M19.7125 13.1647V21.1647'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M25.7125 7.16467V26.1647C25.7125 26.4299 25.6072 26.6842 25.4196 26.8718C25.2321 27.0593 24.9777 27.1647 24.7125 27.1647H8.71252C8.44731 27.1647 8.19295 27.0593 8.00542 26.8718C7.81788 26.6842 7.71252 26.4299 7.71252 26.1647V7.16467'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M21.7125 7.16467V5.16467C21.7125 4.63424 21.5018 4.12553 21.1267 3.75046C20.7517 3.37539 20.243 3.16467 19.7125 3.16467H13.7125C13.1821 3.16467 12.6734 3.37539 12.2983 3.75046C11.9232 4.12553 11.7125 4.63424 11.7125 5.16467V7.16467'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                  </svg>
-                </button>
-              </div>
-            ))
-          ) : toShow ? (
+          {toShow ? (
             toShow.map((e) => (
               <div key={e.id} className={styles.actions}>
                 <button>
@@ -374,10 +274,10 @@ export const PostAdmin = () => {
           )}
         </div>
       </div>
-      <div>
-        <button /* onClick={(e) => prev(e)} */>Prev</button>
-        {/* <Paginate totalPages={cant} setPage= {setPage} /> */}
-        <button /* onClick={(e) => next(e)}  */>Next</button>
+      <div className={styles.buttons}>
+        <button onClick={(e) => prev(e)} >Prev</button>
+        <Paginado itemsP={27} array={filter?.length || array?.length} paginate={paginate} />
+        <button onClick={(e) => next(e)}  >Next</button>
       </div>  
       {deletePost ? <ModalDeletePost id={id} show={handleShow} hidden={show}/> : null}
     </div>
--- src/containers/UsersAdmin/UsersAdmin.js
+++ src/containers/UsersAdmin/UsersAdmin.js
@@ -5,6 +5,7 @@ import { getUsers } from '../../ducks/actions/actionCreators';
 import { ModalPass } from '../../components/Modals/ModalPass';
 import { ModalRol } from '../../components/Modals/ModalRol';
 import { ModalDeleteUser } from '../../components/Modals/ModalDeleteUser';
+import Paginado from '../Post/paginado';
 export const UsersAdmin = () => {
   let Users = useSelector((state) => state.reducer.users);
   const dispatch = useDispatch();
@@ -16,7 +17,24 @@ export const UsersAdmin = () => {
   const [deleteUser, setDeleteUser] = useState(false);
   const [rol, setrol] = useState(0);
   const [id, setId] = useState(null);
+  const [current,setCurrent] = useState(1)
+  const lastIndex = current * 27
+  const first = lastIndex - 27
+  const pages = Math.ceil(filter ? filter?.length /27 : Users.length /27)
+  let toShow = filter ? filter?.slice(first, lastIndex) : Users.slice(first, lastIndex)
+  const paginate = (e) => {
+    setCurrent(e)
+  }
+  const next = (e) => {
+    e.preventDefault();
+    if (current < pages) setCurrent((current) => current + 1);
 
+  };
+  const prev = (e) => {
+    e.preventDefault();
+    if (current > 1) setCurrent((current) => current - 1);
+
+  };
   
   const handleEditPass = (e) => {
     setId(e);
@@ -121,14 +139,8 @@ export const UsersAdmin = () => {
         <div className={styles.boxOne}>Lista de Usuarios</div>
         <div className={styles.boxTwo}>
           <div className={styles.title}>ID User</div>
-          {filter ? (
-            filter?.map((e) => (
-              <div key={e.id} className={styles.item}>
-                {e.id}
-              </div>
-            ))
-          ) : Users ? (
-            Users.map((e) => (
+          {toShow ? (
+            toShow.map((e) => (
               <div key={e.id} className={styles.item}>
                 {e.id}
               </div>
@@ -139,14 +151,8 @@ export const UsersAdmin = () => {
         </div>
         <div className={styles.boxThree}>
           <div className={styles.title}>Username</div>
-          {filter ? (
-            filter?.map((e) => (
-              <div key={e.id} className={styles.item}>
-                {e.username}
-              </div>
-            ))
-          ) : Users ? (
-            Users.map((e) => (
+          {toShow ? (
+            toShow.map((e) => (
               <div key={e.id} className={styles.item}>
                 {e.username}
               </div>
@@ -157,14 +163,8 @@ export const UsersAdmin = () => {
         </div>
         <div className={styles.boxFour}>
           <div className={styles.title}>Email</div>
-          {filter ? (
-            filter?.map((e) => (
-              <div key={e.id} className={styles.item}>
-                {e.email}
-              </div>
-            ))
-          ) : Users ? (
-            Users.map((e) => (
+          {toShow ? (
+            toShow.map((e) => (
               <div key={e.id} className={styles.item}>
                 {e.email}
               </div>
@@ -175,14 +175,8 @@ export const UsersAdmin = () => {
         </div>
         <div className={styles.boxFive}>
           <div className={styles.title} onClick={(e) => changeRol(e)}>Rol</div>
-          {filter ? (
-            filter?.map((e) => (
-              <div key={e.id} className={styles.item}>
-                {e.role}
-              </div>
-            ))
-          ) : Users ? (
-            Users.map((e) => (
+          {toShow ? (
+            toShow.map((e) => (
               <div key={e.id} className={styles.item}>
                 {e.role}
               </div>
@@ -195,14 +189,8 @@ export const UsersAdmin = () => {
           <div className={styles.title}>
             Status
           </div>
-          {filter ? (
-            filter?.map((e) => (
-              <div key={e.id} className={styles.item}>
-                {e.status === true ? 'Activo' : 'Inactivo'}
-              </div>
-            ))
-          ) : Users ? (
-            Users.map((e) => (
+          {toShow ? (
+            toShow.map((e) => (
               <div key={e.id} className={styles.item}>
                 {e.status === true ? 'Activo' : 'Inactivo'}
               </div>
@@ -213,121 +201,8 @@ export const UsersAdmin = () => {
         </div>
         <div className={styles.boxSeven}>
           <div className={styles.title}>Action</div>
-          {filter ? (
-            filter.map((e) => (
-              <div key={e.id} className={styles.actions}>
-                <button key={e.id} onClick={(x) => handleEditPass(e.id)}>
-                  <svg
-                    width='25'
-                    height='25'
-                    viewBox='0 0 33 33'
-                    fill='none'
-                    xmlns='http://www.w3.org/2000/svg'>
-                    <path
-                      d='M12.7125 27.1647H6.71253C6.44731 27.1647 6.19296 27.0593 6.00542 26.8718C5.81789 26.6843 5.71253 26.4299 5.71253 26.1647V20.5772C5.71208 20.4473 5.73723 20.3187 5.78657 20.1986C5.8359 20.0784 5.90844 19.9692 6.00003 19.8772L21 4.87719C21.0931 4.7827 21.204 4.70767 21.3263 4.65645C21.4486 4.60523 21.5799 4.57886 21.7125 4.57886C21.8451 4.57886 21.9764 4.60523 22.0987 4.65645C22.2211 4.70767 22.332 4.7827 22.425 4.87719L28 10.4522C28.0945 10.5452 28.1695 10.6561 28.2208 10.7785C28.272 10.9008 28.2984 11.0321 28.2984 11.1647C28.2984 11.2973 28.272 11.4286 28.2208 11.5509C28.1695 11.6732 28.0945 11.7841 28 11.8772L12.7125 27.1647Z'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M17.7125 8.16467L24.7125 15.1647'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M27.7125 27.1647H12.7125L5.77502 20.2272'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M21.2125 11.6647L9.21252 23.6647'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                  </svg>
-                </button>
-                <button key={e.id} onClick={(x) => handleEditRol(e.id)}>
-                  <svg
-                    width='25'
-                    height='25'
-                    viewBox='0 0 32 32'
-                    fill='none'
-                    xmlns='http://www.w3.org/2000/svg'>
-                    <path
-                      opacity='0.2'
-                      d='M16 20C20.4183 20 24 16.4183 24 12C24 7.58172 20.4183 4 16 4C11.5817 4 8 7.58172 8 12C8 16.4183 11.5817 20 16 20Z'
-                      fill='white'
-                    />
-                    <path
-                      d='M16 20C20.4183 20 24 16.4183 24 12C24 7.58172 20.4183 4 16 4C11.5817 4 8 7.58172 8 12C8 16.4183 11.5817 20 16 20Z'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeMiterlimit='10'
-                    />
-                    <path
-                      d='M3.875 27C5.10367 24.8714 6.87104 23.1038 8.99944 21.8749C11.1278 20.6459 13.5423 19.9989 16 19.9989C18.4577 19.9989 20.8722 20.6459 23.0006 21.8749C25.129 23.1038 26.8963 24.8714 28.125 27'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                  </svg>
-                </button>
-                <button key={e.id} onClick={(x) => handleDeleteUser(e.id)}>
-                  <svg
-                    width='25'
-                    height='25'
-                    viewBox='0 0 33 33'
-                    fill='none'
-                    xmlns='http://www.w3.org/2000/svg'>
-                    <path
-                      d='M27.7125 7.16467H5.71252'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M13.7125 13.1647V21.1647'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M19.7125 13.1647V21.1647'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M25.7125 7.16467V26.1647C25.7125 26.4299 25.6072 26.6842 25.4196 26.8718C25.2321 27.0593 24.9777 27.1647 24.7125 27.1647H8.71252C8.44731 27.1647 8.19295 27.0593 8.00542 26.8718C7.81788 26.6842 7.71252 26.4299 7.71252 26.1647V7.16467'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                    <path
-                      d='M21.7125 7.16467V5.16467C21.7125 4.63424 21.5018 4.12553 21.1267 3.75046C20.7517 3.37539 20.243 3.16467 19.7125 3.16467H13.7125C13.1821 3.16467 12.6734 3.37539 12.2983 3.75046C11.9232 4.12553 11.7125 4.63424 11.7125 5.16467V7.16467'
-                      stroke='white'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                  </svg>
-                </button>
-              </div>
-            ))
-          ) : Users ? (
-            Users.map((e) => (
+          {toShow ? (
+            toShow.map((e) => (
               <div key={e.id} className={styles.actions}>
                 <button key={e.id} onClick={(x) => handleEditPass(e.id)}>
                   <svg
@@ -444,9 +319,10 @@ export const UsersAdmin = () => {
           )}
         </div>
       </div>
-      <div>
-        <button>Prev</button>
-        <button>Next</button>
+      <div className={styles.buttons}>
+        <button onClick={(e) => prev(e)}>Prev</button>
+        <Paginado items={27} array={filter?.length || Users.length} paginate={paginate}/>
+        <button onClick={(e) => next(e)}>Next</button>
       </div>
       {editUser ? <ModalPass id={id} show={handleShow} hidden={show} /> : null}
       {editRol ? <ModalRol id={id} show={handleShow} hidden={show} /> : null}
