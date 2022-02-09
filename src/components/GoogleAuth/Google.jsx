66876f49c7b94fcc1853f4090f7fac7f4dd49081
Alvaro Canales Aguilar <alvarocanales1599@gmail.com>
2022-02-08 20:10:35 -0500

    google fix

--- src/components/GoogleAuth/Google.jsx
+++ src/components/GoogleAuth/Google.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 import { Redirect } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { CLIENT_ID, REDIRECT_URI, SCOPE } from './cred';
 
 function Google() {
+    const onSuccess = response=>{
+        if (!localStorage.getItem('token')) {
+            console.log("Encoded JWT ID token: " + response.tokenId);
+        fetch("http://localhost:4000/api/decoderGoogle?token="+ response.tokenId,{
+          method:"POST"
+        })
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(data) {
+            console.log(data);
+            localStorage.setItem("logged",true)
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('rol', data.rol);
+            localStorage.setItem('username', data.username);
+            localStorage.setItem('userId', data.id)
+            localStorage.setItem('email', data.username);
+            window.location.href="http://localhost:3000/"
+          });
+        }
+    }
+    const onFailure = response=>{
+        console.log("login failed"+ "res:" +response);
+    }
     return (
         <div id="google">
-            <div
-                id='g_id_onload'
-                data-client_id='586990699149-mf4qeqt0f33bine5135ai4t4feibkp2c.apps.googleusercontent.com'
+            <GoogleLogin
+                clientId={CLIENT_ID}
+                buttonText="Login"
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                isSignedIn={true}
                 data-login_uri='http://localhost:4000/api/auth/callback'
                 data-auto_prompt='false'
                 callback='onSignInCallback'
-            />
-            <div
-                class='g_id_signin'
-                data-type='standard'
-                data-size='large'
-                data-theme='outline'
-                data-text='sign_in_with'
-                data-shape='rectangular'
-                data-logo_alignment='left'
             />
         </div>
     );
