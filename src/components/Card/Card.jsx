c5998c44065c1d2f76cb0ee8495a8317e79d5263
c4chris124 <c4chris124@gmail.com>
2022-02-09 12:11:46 -0600

    Adding styles for cards, addig rating, fixing some responsive in cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,20 +9,18 @@ import Rating from "@mui/material/Rating";
 const defaultImage =
   'https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/59bbb29c5bafe878503c9872/husky-siberiano-bosque.jpg';
 
-export default function CardItem({ image, title, details, price, id }) {
-  const rating = 2
+export default function CardItem({ image, title, details, price, id, ratingProm }) {
   return (
     <NavLink to={`detail/${id}`} className={s.link}>
       <Paper elevation={3}>
-        <Card sx={{ width: 300, height: 380 }}>
-          <CardMedia component="img"
-            height="250"
+        <Card className={s.card}>
+          <CardMedia className={s.card_image} component="img"
             image={image || defaultImage}
             alt="post image" />
           <CardContent>
 
-          <Rating name="read-only" value={rating} readOnly />
-          <h3>${price}</h3>
+          <Rating name="read-only" value={ratingProm} readOnly />
+          <h2 className={s.price_title}>${price}</h2>
 
             {title.length < 17 ? (
               <h5>{title}</h5>
