9bf8eb987363e9f9cc226352d362df75dd5e6b02
c4chris124 <c4chris124@gmail.com>
2022-02-09 18:22:23 -0600

    replacing Order for compras, was the same component, also replace routes in app.js in commit before this one

--- src/Views/Order/OrderDetail.jsx
+++ src/Views/Order/OrderDetail.jsx
@@ -1,26 +1,30 @@
 import OrderCard from "../../components/OrderCard/OrderCard";
 import styles from './_Order.module.scss'
 
-const OrderDetail = ({ ids, delivery_adress, status, createdAt, posts }) => {
+const OrderDetail = ({ id, OrderDetail, created, status, total }) => {
+
+
     return (
         <div className={`card w-100 ${styles.order_detail_container}`}>
             <div class="card-header">
-                Order
+            Order ID:<span> {id}</span> 
             </div>
             <ul className="list-group list-group-flush">
-                <li className="list-group-item"><p className="h6">Shipping Address: </p>{delivery_adress}</li>
                 <li className="list-group-item"><p className="h6">Status: </p>{status}</li>
             </ul>
+            <ul className="list-group list-group-flush">
+                <li className={`list-group-item ${styles.total_price}`}><p className="h6">Total: </p>${total}</li>
+            </ul>
             <div className={styles.product}>
-                <h5 className={`h4 ${styles.title_product}`}>Products ({posts.length})</h5>
+                <h5 className={`h4 ${styles.title_product}`}>Products ({OrderDetail.length})</h5>
                 <div className={styles.detail_container}>
-                {posts.map((e) => {
-                    return <OrderCard ids={e.id} name={e.name} price={e.price} />
-                })}
+                    {OrderDetail.map((e) => {
+                        return <OrderCard id={e.posts.id} name={e.posts.name} price={e.posts.price} />
+                    })}
                 </div>
             </div>
             <div className="card-footer">
-                <small className="text-muted"><p>Ordered On: </p>{createdAt}</small>
+                <small className="text-muted"><p>Ordered On: </p>{created}</small>
             </div>
         </div>
     )
