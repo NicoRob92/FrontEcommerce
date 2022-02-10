3cc6b96705769df601ac67de590a311f8dc42e51
Juan David Tovar Montoya <davidtovarmontoya@gmail.com>
2022-02-10 13:37:16 -0500

    Imagenes dinamicas del detail

--- src/components/DetailLeftCard/DetailLeftCard.js
+++ src/components/DetailLeftCard/DetailLeftCard.js
@@ -1,14 +1,19 @@
 import styles from './_DetailLeftCard.module.scss';
+import {useState}  from 'react'
 
 const DetailLeftCard = ({ postById }) => {
+  const [imageView , setimageView]= useState("")
+  function selectImge(url) {
+     setimageView(url)
+  }
   return (
     <div className={`${styles.product_imgs}`}>
       <div className={styles.img_showcase}>
-        <img src={postById?.Images[0]?.link} alt={postById?.name} />
+        <img src={imageView||postById?.Images[0]?.link} alt={postById?.name} />
       </div>
       <div className={styles.img_select}>
         {postById.Images?.map((image, i) => (
-          <div key={i} className={styles.img_item}>
+          <div onClick={()=>selectImge(image.link)} key={i} className={styles.img_item}>
             <img src={image.link} alt={postById?.name} />
           </div>
         ))}
